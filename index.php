8c5e74bf4523c133ba0e49fce8b8379b8850626d
Joshua Kristiansen <jkristiansen@mail.greenriver.edu>
2020-01-17 23:11:37 -0500

    Created nav, container and added content. Updated all files

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require("vendor/autoload.php");
+
+// Instantiate Fat-Free
+$f3 = Base::Instance();
+
+// Define a default route (view)
+$f3 -> route("GET /", function () {
+    $view = new Template();
+    echo $view->render("views/home.html");
+});
+
+// Run Fat-Free
+$f3->run();
