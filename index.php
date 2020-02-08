438d5db9d148eaf15fd1d99bf2c352b2cb88c765
Joshua Kristiansen <jkristiansen@mail.greenriver.edu>
2020-02-07 22:45:28 -0500

    Completed validation, nearly every file updated.

--- index.php
+++ index.php
@@ -8,11 +8,13 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Require autoload file
-require("vendor/autoload.php");
-require("model/validation.php");
+require_once("vendor/autoload.php");
+require_once("model/validation.php");
 
 // Instantiate Fat-Free
 $f3 = Base::Instance();
+
+$f3->set('gender', array('Male', 'Female'));
 
 // Define a default route (view)
 $f3 -> route("GET /", function () {
@@ -28,11 +30,13 @@ $f3->route('GET|POST /sign-up/1', function($f3) {
         $fname = $_POST["fname"];
         $lname = $_POST["lname"];
         $age = $_POST["age"];
+        $gender = $_POST["gender"];
         $pnumber = $_POST["pnumber"];
 
         $f3->set('fname', $fname);
         $f3->set('lname', $lname);
         $f3->set('age', $age);
+        $f3->set('gender', $gender);
         $f3->set('pnumber', $pnumber);
 
         // If data is valid
@@ -42,9 +46,10 @@ $f3->route('GET|POST /sign-up/1', function($f3) {
             $_SESSION['fname'] = $fname;
             $_SESSION['lname'] = $lname;
             $_SESSION['age'] = $age;
+            $_SESSION['gender'] = $gender;
             $_SESSION['pnumber'] = $pnumber;
 
-            // Redirect to Summary
+            // Redirect to part 2
             $f3->reroute('/sign-up/2');
         }
     }
@@ -54,38 +59,79 @@ $f3->route('GET|POST /sign-up/1', function($f3) {
 });
 
 // Define another route
-$f3->route('GET|POST /sign-up/2', function() {
-    // var_dump($_POST);
-    $_SESSION['fname'] = $_POST['fname'];
-    $_SESSION['lname'] = $_POST['lname'];
-    $_SESSION ['age'] = $_POST['age'];
-    $_SESSION['gender'] = $_POST['customRadio'];
-    $_SESSION['pnumber']= $_POST['pnumber'];
+$f3->route('GET|POST /sign-up/2', function($f3) {
+
+    //var_dump($_SESSION);
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $email = $_POST["email"];
+        $state = $_POST["state"];
+        $seeking = $_POST["seeking"];
+        $bio = $_POST["bio"];
+
+        $f3->set('email', $email);
+        $f3->set('email', $email);
+        $f3->set('state', $state);
+        $f3->set('seeking', $seeking);
+        $f3->set('bio', $bio);
+
+        if (validFormTwo()) {
+
+            // Add to SESSION variable
+            $_SESSION['email'] = $email;
+            $_SESSION['state'] = $state;
+            $_SESSION['seeking'] = $seeking;
+            $_SESSION['bio'] = $bio;
+
+            // Redirect to part 3
+            $f3->reroute('/sign-up/3');
+        }
+    }
+
     $view = new Template();
     echo $view->render('views/profile_info.html');
 });
 
 // Define another route
-$f3->route('POST /sign-up/3', function($f3) {
-    // var_dump($_POST);
-    $_SESSION['email'] = $_POST['email'];
-    $_SESSION['state'] = $_POST['state'];
-    $_SESSION['seeking'] = $_POST['customRadio'];
-    $_SESSION['bio'] = $_POST['bio'];
+$f3->route('GET|POST /sign-up/3', function($f3) {
+
+    //var_dump($_SESSION);
 
     $f3->set('in_interests', array("Tv", "Movies", "Cooking", "Board games", "Puzzles", "Reading", "Playing cards", "Video games"));
     $f3->set('out_interests', array("Hiking", "Biking", "Swimming", "Collecting", "Walking", "Climbing"));
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $interestsIn = $_POST['interestsIn'];
+        $interestsOut = $_POST['interestsOut'];
+
+        $f3->set('interestsIn', $interestsIn);
+        $f3->set('interestsOut', $interestsOut);
+
+        if (validFormThree()) {
+
+            // Add to SESSION variable
+            $_SESSION['interestsIn'] = $interestsIn;
+            $_SESSION['interestsOut'] = $interestsOut;
+
+            // Redirect to summary
+            $f3->reroute('/sign-up/summary');
+        }
+    }
+
     $view = new Template();
     echo $view->render('views/interests_info.html');
 });
 
 // Define another route
-$f3->route('POST /sign-up/summary', function() {
+$f3->route('GET|POST /sign-up/summary', function() {
     // var_dump($_POST);
-    $_SESSION['interests'] = $_POST['interests'];
     /*while ($_SESSION['interests']) {
         $f3->set('interests', array('chocolate' => 'Chocolate Mousse', 'vanilla' => 'Vanilla Custard', 'strawberry' => 'Strawberry Shortcake'));
     }*/
+
+    //var_dump($_SESSION);
 
     $view = new Template();
     echo $view->render('views/signup_summary.html');
--- model/validation.php
+++ model/validation.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Validate the form
+/* Validate the first form in the sign-up process
  * @return boolean
  */
 function validFormOne() {
@@ -23,6 +23,11 @@ function validFormOne() {
         $f3->set("errors['age']", "Please enter a valid age (18-80)");
     }
 
+    if (!$f3->get('gender')) {
+        $isValid = false;
+        $f3->set("errors['gender']", "Please select a gender");
+    }
+
     if (!validPN($f3->get('pnumber'))) {
         $isValid = false;
         $f3->set("errors['pnumber']", "Please enter a 10 digit number");
@@ -41,4 +46,61 @@ function validAge($age) {
 
 function validPN($PN) {
     return preg_match('/^[0-9]{9}/', $PN);
+}
+
+// -------------------------------Form 2-------------------------------
+
+/* Validate the second form in the sign-up process
+ * @return boolean
+ */
+function validFormTwo() {
+
+    global $f3;
+    $isValid = true;
+
+    if (!filter_var($f3->get('email'), FILTER_VALIDATE_EMAIL)) {
+        $isValid = false;
+        $f3->set("errors['email']", "Please enter a valid email");
+    }
+
+    if($_POST['state'] == "") {
+        $isValid = false;
+        $f3->set("errors['state']", "Please choose a state");
+    }
+
+    if (!$f3->get('seeking')) {
+        $isValid = false;
+        $f3->set("errors['seeking']", "Please select a gender");
+    }
+
+    if (!$f3->get('bio')) {
+        $isValid = false;
+        $f3->set("errors['bio']", "Please enter a short bio about yourself...");
+    }
+
+    return $isValid;
+}
+
+// -------------------------------Form 3-------------------------------
+
+function validFormThree() {
+
+    global $f3;
+    $isValid = true;
+
+    if ($f3->get('interestsIn')) {
+        if (!array_intersect($f3->get('interestsIn'), $f3->get('in_interests'))) {
+            $isValid = false;
+            $f3->set("errors['in_interest']", "Oops... An option you chose is invalid. Please choose from the provided options");
+        }
+    }
+
+    if ($f3->get('interestsOut')) {
+        if (!array_intersect($f3->get('interestsOut'), $f3->get('out_interests'))) {
+            $isValid = false;
+            $f3->set("errors['out_interest']", "Oops... An option you chose is invalid. Please choose from the provided options");
+        }
+    }
+
+    return $isValid;
 }
